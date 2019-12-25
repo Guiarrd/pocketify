235d351d64b8a9df0e1a4cdc7b3108d486d4d556
Guilherme Arruda <guilherme_arruda@live.com>
2019-12-25 17:10:45 -0300

    Criação da dashboard e melhorias no layout

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'home#index'
+
+  # dá pra ter mais de uma rota root, desde que sejam utilizados
+  # os métodos authenticated do devise.
+  authenticated :user do
+    root to: "dashboard#index", as: :authenticated_root
+  end
+
+  unauthenticated :user do
+    root to: "home#index"
+  end
 end

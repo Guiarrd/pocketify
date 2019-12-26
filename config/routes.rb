ec31aeeb6bb8190176043301667d173e9c61563a
Guilherme Arruda <guilherme_arruda@live.com>
2019-12-26 01:34:57 -0300

    Criação das telas de pesquisa

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
+  get 'search/index'
+  get 'search/new'
   devise_for :users
 
   # dá pra ter mais de uma rota root, desde que sejam utilizados
   # os métodos authenticated do devise.
   authenticated :user do
     root to: "dashboard#index", as: :authenticated_root
+    resources :search, only: [:index, :new], as: :searches
   end
 
   unauthenticated :user do
